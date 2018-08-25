170aea88f4f2dddb46b4005898a636cb9fabe08b
Sebastian Grail <sgrail@gmail.com>
2018-08-26 08:45:07 +1000

    Add Point and empty test

---
+++ Property Based Testing Workshop/Property Based Testing Workshop/Point.swift
@@ -0,0 +1,18 @@
+//
+//  Point.swift
+//  Property Based Testing Workshop
+//
+//  Created by Sebastian Grail on 26/8/18.
+//  Copyright © 2018 Sebastian Grail. All rights reserved.
+//
+
+import Foundation
+
+struct Point {
+    var x: Int
+    var y: Int
+    
+    mutating func shiftBy(dx: Int, dy: Int) {
+        fatalError()
+    }
+}
