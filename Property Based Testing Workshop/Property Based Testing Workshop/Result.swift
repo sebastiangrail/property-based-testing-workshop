9e6365fd7a1eb5abee649f3ebd03ebd55c6a981b
Sebastian Grail <sgrail@gmail.com>
2018-08-27 18:42:11 +1000

    Add implementation and solutions for Result

--- Property Based Testing Workshop/Property Based Testing Workshop/Result.swift
+++ Property Based Testing Workshop/Property Based Testing Workshop/Result.swift
@@ -13,6 +13,24 @@ public enum Result<Value, Error: Swift.Error> {
     case failure(Error)
     
     func map<T>(_ transform: (Value) -> T) -> Result<T, Error> {
-        fatalError()
+        switch self {
+        case .success(let value):
+            return .success(transform(value))
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
+
+extension Result: Equatable where Value: Equatable, Error: Equatable {
+    public static func ==(lhs: Result, rhs: Result) -> Bool {
+        switch (lhs, rhs) {
+        case (.success(let left), .success(let right)):
+            return left == right
+        case (.failure(let left), .failure(let right)):
+            return left == right
+        case (.success, _), (.failure, _):
+            return false
+        }
     }
 }
--- Property Based Testing Workshop/Property Based Testing WorkshopTests/ResultTests.swift
+++ Property Based Testing Workshop/Property Based Testing WorkshopTests/ResultTests.swift
@@ -2,9 +2,42 @@ import XCTest
 import SwiftCheck
 @testable import Property_Based_Testing_Workshop
 
+extension Result: Arbitrary where Value: Arbitrary, Error: Arbitrary {
+    public static var arbitrary: Gen<Result<Value, Error>> {
+        return Gen.compose { composer -> Result<Value, Error> in
+            if composer.generate() {
+                let value: Value = composer.generate()
+                return Result.success(value)
+            } else {
+                let error: Error = composer.generate()
+                return Result.failure(error)
+            }
+        }
+    }
+    
+    public static func shrink(_ result: Result) -> [Result] {
+        switch result {
+        case .success(let value):
+            return Value.shrink(value).map { Result.success($0)}
+        case .failure(let error):
+            return Error.shrink(error).map { Result.failure($0)}
+        }
+    }
+}
+
+
 class ResultTests: XCTestCase {
     
     func testFunctorLaws() {
+        property("mapping identity is identity") <- forAll { (x: Result<Int, String>) in
+            x.map { $0 } == x
+        }
+        
+        property("map(f•g) = map(f)•map(g)") <- forAll { (x: Result<Int, String>, arrowF: ArrowOf<String, Int>, arrowG: ArrowOf<Int, String>) in
+            let f = arrowF.getArrow
+            let g = arrowG.getArrow
+            return x.map { f(g($0)) } == x.map(g).map(f)
+        }
     }
     
 }
